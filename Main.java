3eee6f8f00e5fab8f837570da79968003e610ea7
Artur <106569140+arturbertolini@users.noreply.github.com>
2025-05-24 16:56:09 -0300

    Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.seuUsuario.board;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bem-vindo ao projeto Board Java Console!");
+        Game game = new Game();
+        game.start();
+    }
+}
